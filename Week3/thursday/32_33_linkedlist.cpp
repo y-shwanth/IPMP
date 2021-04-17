769aa26204170d14512dfe87792c59f2ff840a97
y-shwanth <63255085+y-shwanth@users.noreply.github.com>
2021-04-17 23:52:39 +0530

    Add files via upload

---
+++ Week3/thursday/32_33_linkedlist.cpp
@@ -0,0 +1,63 @@
+
+/* C++ Implementation of Memory
+efficient Doubly Linked List */
+#include <bits/stdc++.h>
+#include <cinttypes>
+using namespace std;
+ 
+class Node
+{
+    public:
+    int data;
+    Node* npx; 
+};
+
+Node* XOR (Node *a, Node *b)
+{
+    return reinterpret_cast<Node *>(
+      reinterpret_cast<uintptr_t>(a) ^
+      reinterpret_cast<uintptr_t>(b));
+}
+ 
+void insert(Node **head_ref, int data)
+{
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->npx = *head_ref;
+ 
+    if (*head_ref != NULL)
+    {
+        (*head_ref)->npx = XOR(new_node, (*head_ref)->npx);
+    }
+ 
+    *head_ref = new_node;
+}
+ 
+void printList (Node *head)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next;
+ 
+    cout << "Nodes of Linked List: \n";
+    while (curr != NULL)
+    {
+        cout<<curr->data<<" ";
+        next = XOR (prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+}
+ 
+int main ()
+{
+    Node *head = NULL;
+    insert(&head, 10);
+    insert(&head, 20);
+    insert(&head, 30);
+    insert(&head, 40);
+
+    printList (head);
+ 
+    return (0);
+}
