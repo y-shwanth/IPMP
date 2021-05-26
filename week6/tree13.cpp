088f0f5424d3004bf342f35a70ad88f5a6fda630
y-shwanth <63255085+y-shwanth@users.noreply.github.com>
2021-05-26 20:29:45 +0530

    Update tree13.cpp

--- week6/tree13.cpp
+++ week6/tree13.cpp
@@ -1,1 +1,44 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diffbtwoddeven(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+      
+        queue<TreeNode*> oldq,newq;
+        TreeNode* temp = NULL;
+        oldq.push(root);
+        int flag = -1,x = 0;
+        while(true)
+        {
+           TreeNode* temp = oldq.front();
+            x = x + flag*temp->val;
+            oldq.pop();
+            if(temp->left != NULL)
+                newq.push(temp->left);
+            if(temp->right != NULL)
+                newq.push(temp->right);
+            
+            if(oldq.empty() == true)
+            {
+                flag = flag*-1;
+                if(newq.empty() == true)
+                    break;
+                swap(oldq,newq);
+                newq = queue<TreeNode*>();
+            }
+            
+        }
+        return x;
+    }
+};
