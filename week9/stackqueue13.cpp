d345f0c84bc95844bb5585a246cce7bdf9da4787
y-shwanth <63255085+y-shwanth@users.noreply.github.com>
2021-06-14 16:28:29 +0530

    Create stackqueue13.cpp

---
+++ week9/stackqueue13.cpp
@@ -0,0 +1,56 @@
+/* INITIAL APPROACH
+class Solution {
+public:
+    int find_newmax(queue<int> q)
+    {
+        int max = INT_MIN;
+        while(q.empty() == false)
+        {
+            if(q.front() > max)
+                max = q.front();
+            
+            q.pop();
+        }
+        return max;
+    }
+    
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        queue<int> q;
+        int max = INT_MIN;
+        
+        for(int i=0;i<k;i++)
+        {
+            if(nums[i] > max)
+                max = nums[i];
+            
+            q.push(nums[i]);
+        }
+        
+        vector<int> max_arr;
+        max_arr.push_back(max);
+        
+        for(int i=k;i<nums.size();i++)
+        {
+            q.push(nums[i]);
+            if(nums[i] > max)
+            {
+                max = nums[i];
+                q.pop();
+            }
+            else if(max == q.front())
+            {
+                q.pop();
+                max = find_newmax(q);
+            }
+            else
+            {
+                q.pop();
+            }
+            
+            max_arr.push_back(max);
+        }
+        
+        return max_arr;
+    }
+};
+*/
