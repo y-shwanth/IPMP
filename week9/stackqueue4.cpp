5c4ced9e16550ae1bc3ccb8388cfbd3d771a5927
y-shwanth <63255085+y-shwanth@users.noreply.github.com>
2021-06-15 12:38:41 +0530

    Update stackqueue4.cpp

--- week9/stackqueue4.cpp
+++ week9/stackqueue4.cpp
@@ -1,29 +1,55 @@
-class Solution {
-public: 
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        vector<int> v;
-        deque<int> q;
-        for(int i=0;i<k;i++)
+class MinStack {
+public:
+    stack<long long int> s;
+    long long int min;
+    /** initialize your data structure here. */
+    MinStack() {
+    }
+    
+    void push(int val) {
+        long long int val1 = val;
+        if(s.empty())
         {
-            while(!q.empty() && nums[q.back()] < nums[i])
-                q.pop_back();
-            
-            q.push_back(i);
+            min = val;
+            s.push(val);
+            return;
         }
-        
-        for(int i=k;i<nums.size();i++)
+        if(val < min)
         {
-            v.push_back(nums[q.front()]);
-            while(!q.empty() && q.front() <= i - k)
-                q.pop_front();
-            
-            while(!q.empty() && nums[q.back()] < nums[i])
-                q.pop_back();
-            
-            q.push_back(i);
+            s.push((2*val1) - min);
+            min = val;
+            return;
         }
-        v.push_back(nums[q.front()]);
-        return v;
+        s.push(val);
+    }
+    
+    void pop() {
+        if(s.top() >= min)
+            s.pop();
+        else
+        {
+            min = 2*min - s.top();
+            s.pop();
+        }
+    }
+    
+    int top() {
+        if(s.top() >= min)
+            return s.top();
+        
+        return min;
+    }
+    
+    int getMin() {
+        return min;
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
