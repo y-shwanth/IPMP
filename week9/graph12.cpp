75e8ef159b344dedd6dc38c080ae13a23364d574
y-shwanth <63255085+y-shwanth@users.noreply.github.com>
2021-06-18 22:24:32 +0530

    Create graph12.cpp

---
+++ week9/graph12.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Edge {
+	int src, dest;
+};
+
+struct Graph {
+	int V, E;
+	struct Edge* edge;
+};
+
+struct subset {
+	int parent;
+	int rank;
+};
+
+struct Graph* createGraph(int V, int E)
+{
+	struct Graph* graph
+		= (struct Graph*)malloc(sizeof(struct Graph));
+	graph->V = V;
+	graph->E = E;
+
+	graph->edge = (struct Edge*)malloc(
+		graph->E * sizeof(struct Edge));
+
+	return graph;
+}
+
+int find(struct subset subsets[], int i)
+{
+	if (subsets[i].parent != i)
+		subsets[i].parent
+			= find(subsets, subsets[i].parent);
+
+	return subsets[i].parent;
+}
+
+void Union(struct subset subsets[], int xroot, int yroot)
+{
+	if (subsets[xroot].rank < subsets[yroot].rank)
+		subsets[xroot].parent = yroot;
+	else if (subsets[xroot].rank > subsets[yroot].rank)
+		subsets[yroot].parent = xroot;
+
+	else {
+		subsets[yroot].parent = xroot;
+		subsets[xroot].rank++;
+	}
+}
+
+int isCycle(struct Graph* graph)
+{
+	int V = graph->V;
+	int E = graph->E;
+
+	struct subset* subsets
+		= (struct subset*)malloc(V * sizeof(struct subset));
+
+	for (int v = 0; v < V; ++v) {
+		subsets[v].parent = v;
+		subsets[v].rank = 0;
+	}
+
+	for (int e = 0; e < E; ++e) {
+		int x = find(subsets, graph->edge[e].src);
+		int y = find(subsets, graph->edge[e].dest);
+
+		if (x == y)
+			return 1;
+
+		Union(subsets, x, y);
+	}
+	return 0;
+}
+
+int main()
+{
+	int V = 3, E = 3;
+	struct Graph* graph = createGraph(V, E);
+
+	graph->edge[0].src = 0;
+	graph->edge[0].dest = 1;
+
+	graph->edge[1].src = 1;
+	graph->edge[1].dest = 2;
+
+	graph->edge[2].src = 0;
+	graph->edge[2].dest = 2;
+
+	if (isCycle(graph))
+		printf("Graph contains cycle");
+	else
+		printf("Graph doesn't contain cycle");
+
+	return 0;
+}
